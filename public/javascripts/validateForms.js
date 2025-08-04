5d6365357f8ef6704b3a83c590db3aac51e79255
krish2213 <krishshree2209@gmail.com>
2025-08-04 20:57:30 +0530

    Username Validation added

--- public/javascripts/validateForms.js
+++ public/javascripts/validateForms.js
@@ -5,6 +5,14 @@
 
     Array.from(forms).forEach(function (form) {
         form.addEventListener('submit', function (event) {
+            const username = form.querySelector('input[name="username"]');
+            const usernamePattern = '/^[A-Za-z0-9_]+$';
+            if (username && !usernamePattern.test(username.value)) {
+                username.setCustomValidity('Username can only contain letters, numbers, and underscores.');
+            } 
+            else{
+                username.setCustomValidity('');
+            }
             if (!form.checkValidity()) {
                 event.preventDefault()
                 event.stopPropagation()
